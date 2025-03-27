fa52d8fa6c96e876d141340cb2c13d30a7fbc163
CPU-Digo <168138711+CPU-Digo@users.noreply.github.com>
2025-03-27 17:26:58 -0300

    Add files via upload

---
+++ src/Funcionario.java
@@ -0,0 +1,30 @@
+public class Funcionario {
+    private String nome;
+    private double salarioBase;
+    private int horasTrabalhadas;
+
+
+    public Funcionario(String nome, double salarioBase, int horasTrabalhadas){
+        this.nome = nome;
+        this.salarioBase = salarioBase;
+        this.horasTrabalhadas = horasTrabalhadas;
+    }
+
+    public double calcularSalarioFinal(){
+        int horasNormais = 160;
+        double valorHora = salarioBase / horasNormais;
+        if(horasTrabalhadas > horasNormais){
+            int horasExtras = horasTrabalhadas - horasNormais;
+            return salarioBase + (horasExtras * valorHora * 1.5);
+        }
+        return salarioBase;
+    }
+
+    public void exibirDados() {
+        System.out.printf("Funcionario: %s\n",nome);
+        System.out.printf("Salário Base: R$%2f\n", salarioBase);
+        System.out.printf("Horas Trabalhadas: %d\n", horasTrabalhadas);
+        System.out.printf("Salário Final: R$%2f\n", calcularSalarioFinal());
+        System.out.println("----------------------");
+    }
+}
